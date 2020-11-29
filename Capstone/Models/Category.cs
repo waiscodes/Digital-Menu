7c911e674c51b69930df4fbcd19f77de1c23e1c1
waiscodes <b.wais@outlook.com>
2020-11-29 09:56:38 -0700

    added a categories table

---
+++ Capstone/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Capstone.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(45)")]
+        public string Name { get; set; }
+    }
+}
