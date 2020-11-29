01d7edb1fe43c448e65154b73fa778dedaf6b298
waiscodes <b.wais@outlook.com>
2020-11-29 09:45:33 -0700

    added a Restaurant table. Decided raustant table is better than users table.

---
+++ Capstone/Models/Restaurant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Capstone.Models
+{
+    [Table("Restaurant")]
+    public class Restaurant
+    {
+        public Restaurant()
+        {
+        }
+
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(75)")]
+        public string ResName { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string ResUsername { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "varchar(75)")]
+        public string ResLocation { get; set; }
+    }
+}
