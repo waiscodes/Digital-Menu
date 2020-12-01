14d712175bb50319f1237889815d8b0a05e72407
waiscodes <b.wais@outlook.com>
2020-12-01 11:11:48 -0700

    added a footer component with a copyright

---
+++ Capstone/ClientApp/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>&copy; 2020 Don't Panic</p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
