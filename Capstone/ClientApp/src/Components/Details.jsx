ffea3f9a8b023f1022da09a00dcbfa33b3a8389f
waiscodes <b.wais@outlook.com>
2020-11-30 18:08:54 -0700

    imported axios, added axios method and rendered data in JSON format

--- Capstone/ClientApp/src/Components/Details.jsx
+++ Capstone/ClientApp/src/Components/Details.jsx
@@ -1,13 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Details = () => {
-  const { ID } = useParams();
+  // const { ID } = useParams();
   const [menuItem, setMenuItem] = useState();
   const [loading, setLoading] = useState(true);
 
+  const renderDetails = (menuItem) => {
+    return (
+      <>
+        <pre>
+          {JSON.stringify(menuItem, null, 2)}
+        </pre>
+      </>
+    );
+  }
+
+  const populateDetails = async () => {
+    const response = await axios({
+      method: 'get',
+      url: 'Values/GetMenuItem',
+      params: {
+        id : -1
+      }
+    });
+    setMenuItem(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    populateDetails();
+  }, [loading]);
+
+  let content = loading
+    ? <p>Loading...</p>
+    : renderDetails(menuItem);
+
   return (
     <>
+      {content}
     </>
   );
 }
--- Capstone/ClientApp/src/index.js
+++ Capstone/ClientApp/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom';
+import Details from './Components/Details';
 
 ReactDOM.render(
   <React.StrictMode>
     <>
+    <Details />
     </>
   </React.StrictMode>,
   document.getElementById('root')
