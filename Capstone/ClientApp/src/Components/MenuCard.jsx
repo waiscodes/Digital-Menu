be263c9f5a4a8d5da96f564c2256597bb86be8ab
waiscodes <b.wais@outlook.com>
2020-11-30 20:07:57 -0700

    imported axios and set usestate variables

--- Capstone/ClientApp/src/Components/MenuCard.jsx
+++ Capstone/ClientApp/src/Components/MenuCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const MenuCard = () => {
+  // const {username} => useParams();
+  const [menuItems, setMenuItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
+
     </>
   );
 }
