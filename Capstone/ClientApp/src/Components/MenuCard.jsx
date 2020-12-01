2eeb78f09defbe2faa4d525b283530c5a6863f07
waiscodes <b.wais@outlook.com>
2020-11-30 20:21:38 -0700

    added axios and rendered response JSON

--- Capstone/ClientApp/src/Components/MenuCard.jsx
+++ Capstone/ClientApp/src/Components/MenuCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -7,9 +7,43 @@ const MenuCard = () => {
   const [menuItems, setMenuItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const renderMenuItems = (menuItems) => {
+    return (
+      <>
+        {menuItems.map(item => {
+          return (
+            <pre>
+              {JSON.stringify(item, null, 2)}
+            </pre>
+          )
+        })}
+      </>
+    )
+  }
+
+  const populateMenuItems = async () => {
+    const response = await axios({
+      method: 'get',
+      url: 'Values/ListMenu',
+      params: {
+        username: 'Milliways'
+      }
+    });
+    setMenuItems(response.data);
+    setLoading(false);
+  }
+  
+  useEffect(() => {
+    populateMenuItems();
+  }, [loading]);
+
+  let content = loading
+    ? <p>Loading...</p>
+    : renderMenuItems(menuItems);
+
   return (
     <>
-
+      {content}
     </>
   );
 }
--- Capstone/ClientApp/src/index.js
+++ Capstone/ClientApp/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom';
 import Details from './Components/Details';
+import MenuCard from './Components/MenuCard';
 
 ReactDOM.render(
   <React.StrictMode>
     <>
-    <Details />
+    <MenuCard />
     </>
   </React.StrictMode>,
   document.getElementById('root')
