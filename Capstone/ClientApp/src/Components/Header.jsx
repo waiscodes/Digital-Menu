7ad32ea9ddad5916383a46b7c96dd27bec976820
waiscodes <b.wais@outlook.com>
2020-12-02 15:17:10 -0700

    added a condition to only show certain nav items if user is logged in

--- Capstone/ClientApp/src/Components/Header.jsx
+++ Capstone/ClientApp/src/Components/Header.jsx
@@ -1,28 +1,54 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Create'>Create</Link>
-            </li>
-            <li>
-              <Link to='/Login'>Logout</Link>
-            </li>
-            <li>
-              <Link to='/Register'>Register</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  );
+const Header = (props) => {
+  let username = "";
+  if (props.activeUser !== undefined) {
+    username = props.activeUser.username;
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/m/" + username}>Home</Link>
+              </li>
+              <li>
+                <Link to='/Create'>Create</Link>
+              </li>
+              <li>
+                <Link to='/Login'>Logout</Link>
+              </li>
+              <li>
+                <Link to='/Register'>Register</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/Login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/Register'>Register</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    );
+  }
 };
-export default Header;
+export default connect((state) => {
+  return {
+    activeUser: state,
+  };
+})(Header);
