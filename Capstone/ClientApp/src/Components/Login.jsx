a8ad6200c076e224c611ed6361393abce8e943d3
waiscodes <b.wais@outlook.com>
2020-12-01 17:26:25 -0700

    changed login endpoint from get to post. and added axios. tested. minimal validation

--- Capstone/ClientApp/src/Components/Login.jsx
+++ Capstone/ClientApp/src/Components/Login.jsx
@@ -1,20 +1,70 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [response, setResponse] = useState([]);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [waiting, setWaiting] = useState("");
+
+  const handleFieldChange = (e) => {
+    switch (e.target.id) {
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWaiting(true);
+
+    axios({
+      method: "post",
+      url: "Values/Login",
+      headers: { "Content-Type": "multipart/form-data" },
+      params: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((res) => {
+        setWaiting(false);
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        setWaiting(false);
+        setResponse(err.response.data);
+      });
+  };
 
   return (
     <>
-      <form>
-        <h3>Login Restaurant</h3>
+      <form onSubmit={handleSubmit}>
+        <h3>Login</h3>
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            onChange={handleFieldChange}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>password</label>
-          <input type='password' name='password' id='password' />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            onChange={handleFieldChange}
+          />
+        </div>
+        <div>
+          <input type='submit' value='Submit' />
         </div>
       </form>
     </>
