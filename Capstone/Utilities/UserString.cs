4c3bcbf6a70a7c969e958231fa358d60f369e869
waiscodes <b.wais@outlook.com>
2020-12-04 17:32:15 -0700

    created a length comparing utility method for strings

--- Capstone/Utilities/UserString.cs
+++ Capstone/Utilities/UserString.cs
@@ -3,5 +3,16 @@ namespace Capstone.Utilities
 {
     public static class UserString
     {
+        public static bool IsLessThanOrEqualTo(int length, string str)
+        {
+            if (str.Length <= length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
