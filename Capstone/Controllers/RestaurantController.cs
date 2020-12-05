e678a38e22fc0b3c8193aa667b85cbcce08dad7b
waiscodes <b.wais@outlook.com>
2020-12-05 10:08:04 -0700

    trimmed all user input before testing it

--- Capstone/Controllers/RestaurantController.cs
+++ Capstone/Controllers/RestaurantController.cs
@@ -15,17 +15,21 @@ namespace Capstone.Controllers
         // CREATE
         public string Register(string resName, string resUsername, string email, string password, string resLocation)
         {
+            resName = resName.Trim();
             if (UserStr.IsLengthOverLimit(75, resName)) throw new Exception("Restaurant Name cannot exceed excede 75 Characters");
-       
+
+            resUsername = resUsername.Trim();
             if (UserStr.IsLengthOverLimit(75, resUsername)) throw new Exception("Restaurant Username cannot exceed 75 Characters");
             if (resUsername.Contains(" ")) throw new Exception("Username cannot contain a space");
             if (UserStr.ContainsSpecialChar(resUsername)) throw new Exception("Username cannot contain special characters");
 
+            email = email.Trim();
             if (UserStr.IsLengthOverLimit(64, email)) throw new Exception("Email cannot exceed 64 Characters");
             if (!UserStr.IsValidEmail(email)) throw new Exception("Please enter a valid email address");
 
             if (UserStr.IsLengthOverLimit(50, password)) throw new Exception("Password cannot exceed 50 Characters");
 
+            resLocation = resLocation.Trim();
             if (UserStr.IsLengthOverLimit(75, resLocation)) throw new Exception("Address cannot exceed 75 Characters");
 
             using (RestaurantContext context = new RestaurantContext())
