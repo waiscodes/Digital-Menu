66c3df62f3c81983501e2a4be55d2f28049020e4
waiscodes <b.wais@outlook.com>
2020-12-05 11:58:45 -0700

    created an int validation class and added a positive number method

---
+++ Capstone/Utilities/UserInt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Capstone.Utilities
+{
+    public class UserInt
+    {
+        public static bool IsPositiveNumber(int num)
+        {
+            if (num >= 0) return true;
+            else return false;
+        }
+    }
+}
