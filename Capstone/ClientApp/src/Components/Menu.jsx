2c6455d51665bed65d00f9d050b6b1da19d5ad48
waiscodes <b.wais@outlook.com>
2020-12-07 14:59:34 -0700

    imported css into component and added borders for testing

--- Capstone/ClientApp/src/Components/Menu.jsx
+++ Capstone/ClientApp/src/Components/Menu.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import "../css/Menu.css";
 
 const Menu = () => {
   const { username } = useParams();
