1a0130b4b6bad01abb8ffa7aa588f926acb677cb
waiscodes <b.wais@outlook.com>
2020-12-08 16:48:18 -0700

    added description to usreint ulility file

--- Capstone/Utilities/UserInt.cs
+++ Capstone/Utilities/UserInt.cs
@@ -3,6 +3,9 @@ namespace Capstone.Utilities
 {
     public class UserInt
     {
+        /* User input validation for numbers
+         * This was meant to keep the code dry. Only ended up being one method but more will be added here.
+         */
         public static bool IsPositiveNumber(double num)
         {
             if (num >= 0) return true;
