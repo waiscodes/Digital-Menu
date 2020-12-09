3662d4f51ff3a1b9e23a21df23bab8f556d8f70d
waiscodes <b.wais@outlook.com>
2020-12-08 19:32:14 -0700

    Added an exception for empty strings

--- Capstone/Controllers/RestaurantController.cs
+++ Capstone/Controllers/RestaurantController.cs
@@ -19,20 +19,25 @@ namespace Capstone.Controllers
         // CREATE
         public string Register(string resName, string resUsername, string email, string password, string resLocation)
         {
+            if (string.IsNullOrWhiteSpace(resName)) throw new Exception("Restaurant Name cannot be empty");
             resName = resName.Trim();
-            if (UserStr.IsLengthOverLimit(75, resName)) throw new Exception("Restaurant Name cannot exceed excede 75 Characters");
+            if (UserStr.IsLengthOverLimit(75, resName)) throw new Exception("Restaurant Name cannot exceed 75 Characters");
 
+            if (string.IsNullOrWhiteSpace(resUsername)) throw new Exception("Restaurant Username cannot be empty");
             resUsername = resUsername.Trim().ToLower();
             if (UserStr.IsLengthOverLimit(75, resUsername)) throw new Exception("Restaurant Username cannot exceed 75 Characters");
             if (resUsername.Contains(" ")) throw new Exception("Username cannot contain a space");
             if (UserStr.ContainsSpecialChar(resUsername)) throw new Exception("Username cannot contain special characters");
 
+            if (string.IsNullOrWhiteSpace(email)) throw new Exception("Email cannot be empty");
             email = email.Trim();
             if (UserStr.IsLengthOverLimit(64, email)) throw new Exception("Email cannot exceed 64 Characters");
             if (!UserStr.IsValidEmail(email)) throw new Exception("Please enter a valid email address");
 
+            if (string.IsNullOrWhiteSpace(password)) throw new Exception("Password cannot be empty");
             if (UserStr.IsLengthOverLimit(50, password)) throw new Exception("Password cannot exceed 50 Characters");
 
+            if (string.IsNullOrWhiteSpace(resLocation)) throw new Exception("Restaurant Location cannot be empty");
             resLocation = resLocation.Trim();
             if (UserStr.IsLengthOverLimit(75, resLocation)) throw new Exception("Address cannot exceed 75 Characters");
 
